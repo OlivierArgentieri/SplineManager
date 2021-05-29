59d9f92ce40b14bf9e62a7f4fff4e2dad215308f
OlivierArgentieri <olivierargentieri@gmail.com>
2021-05-29 23:27:53 +0200

    clean project

--- Unity_SplineManager/Assets/Unity_Framework/Scripts/Import/Interface/IIsValid.cs
+++
@@ -1,9 +0,0 @@
-namespace Unity_Framework.Scripts.Import.Interface
-{
-    public interface IIsValid
-    {
-        #region f/p
-        bool IsValid { get; }
-        #endregion
-    }
-}
--- Unity_SplineManager/Assets/Unity_SplineManager/Scripts/Import/Interface/IIsValid.cs
+++
@@ -1,9 +0,0 @@
-namespace Unity_SplineManager.Scripts.Import.Interface
-{
-    public interface IIsValid
-    {
-        #region f/p
-        bool IsValid { get; }
-        #endregion
-    }
-}
